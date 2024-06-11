d25cd5597f7d0ffb4f8b48a0049b3bcb54d0d6a6
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-06-11 17:04:06 +0100

    done

--- account/models.py
+++ account/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+from account.utility import generate_account_number
+
+
 # Create your models here.
-
-
 
 class Account(models.Model):
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
     pin = models.CharField(max_length=20)
-    accountNumber = models.CharField(max_length=20, unique=True)
+    accountNumber = models.CharField(max_length=15, default=generate_account_number, unique=True, primary_key=True)
