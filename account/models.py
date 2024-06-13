5d14d5fb0393cef07530e33cb04afc15d625b4f2
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-06-13 11:47:20 +0100

    06-13-2024

--- account/models.py
+++ account/models.py
@@ -9,4 +9,48 @@ class Account(models.Model):
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
     pin = models.CharField(max_length=20)
-    accountNumber = models.CharField(max_length=15, default=generate_account_number, unique=True, primary_key=True)
+    accountNumber = models.CharField(max_length=15,
+                                     default=generate_account_number,
+                                     unique=True,
+                                     primary_key=True)
+    balance = models.DecimalField(max_digits=10,
+                                  decimal_places=2,
+                                  default=0.00)
+    ACCOUNT_TYPE = [
+        ('SAVINGS', 'S'),
+        ('CURRENT', 'C'),
+        ('DOMICILIARY', 'DOM'),
+
+    ]
+    account_type = models.CharField(max_length=1,
+                                    choices=ACCOUNT_TYPE,
+                                    default='SAVINGS')
+
+
+class Transaction(models.Model):
+    TRANSACTION_TYPE = [
+        ('CREDIT',   'CRE'),
+        ('DEBIT',    'TRA'),
+        ('TRANSFER', 'TRAN'),
+
+    ]
+    TRANSACTION_STATUS = [
+        ('SUCCESSFUL', 'S'),
+        ('FAILED',     'F'),
+        ('PENDING',    'P')
+
+    ]
+
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=3,
+                                        choices=TRANSACTION_TYPE,
+                                        default='CRE')
+
+    transaction_date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10
+
+                                 , decimal_places=2)
+    description = models.TextField()
+    transaction_status = models.CharField(max_length=1,
+                                          choices=TRANSACTION_STATUS,
+                                          default='SUCCESSFUL')
