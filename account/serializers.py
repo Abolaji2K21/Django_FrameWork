b5c74b064c86668671825f118d238ed30679354e
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-06-26 11:28:55 +0100

    Account_Serializer

--- account/serializers.py
+++ account/serializers.py
@@ -7,3 +7,9 @@ class AccountSerialize(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = ['accountNumber', 'firstName', 'lastName', 'balance', 'account_type']
+
+
+class AccountCreateSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['pin', 'firstName', 'lastName', 'account_type']
--- account/views.py
+++ account/views.py
@@ -6,20 +6,28 @@ from rest_framework.response import Response
 from rest_framework.templatetags.rest_framework import data
 
 from .models import Account
-from .serializers import AccountSerialize
+from .serializers import AccountSerialize, AccountCreateSerialize
 
 
 # Create your views here.
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def list_account(request):
-    try:
-        accounts = Account.objects.all()
-        serializer = AccountSerialize(accounts, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    except Account.DoesNotExist:
-        return Response(data("Message Account does not exit"), status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        try:
+            accounts = Account.objects.all()
+            serializer = AccountSerialize(accounts, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Account.DoesNotExist:
+            return Response(data("Message Account does not exit"), status=status.HTTP_404_NOT_FOUND)
+    elif request.method == 'POST':
+        serializer = AccountCreateSerialize(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
 
 
 @api_view()
