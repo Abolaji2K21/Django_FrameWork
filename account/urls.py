97b85364c01b9a83013116e56205f2c538c7add8
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-06-27 10:26:17 +0100

    Account-Details

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from account import views
 
 urlpatterns = [
     path('accounts/', views.ListAccount.as_view()),
-    path('accounts/<str:pk>/', views.account_details),
+    path('accounts/<str:pk>/', views.AccountDetail.as_view()),
     path('deposit', views.deposit),
     path('withdraw', views.withdraw)
 
