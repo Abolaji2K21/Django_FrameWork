3fd2eb9e394afbbf35c0dc55568b58151da0277c
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-07-05 11:57:26 +0100

    .dev

--- accountManagementSystem/settings/dev.py
+++ accountManagementSystem/settings/dev.py
@@ -1,7 +1,23 @@
 from .general import *
 
-
 SECRET_KEY = 'django-insecure-h-2)+p87-xvo(m+yz!n1kt^74mcyp=utv$=^w&=vyl1)zge7r*'
 
-
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'account_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
+}
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "localhost"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = "info@agudabank.com"
--- accountManagementSystem/settings/prod.py
+++ accountManagementSystem/settings/prod.py
@@ -7,3 +7,21 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['NAME'],
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': os.environ['HOST'],
+        'PORT': os.environ['PORT']
+    }
+}
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = os.environ['EMAIL_PORT']
+DEFAULT_FROM_EMAIL = os.environ["info@agudabank.com"]
